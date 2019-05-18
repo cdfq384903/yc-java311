54033a6ee3d4185e0e577d8012a97f9d786527cf
蔡育承 <ntycifc0413@gmail.com>
2019-05-18 08:05:05 +0800

    lab3 test

--- src/lab4.java
+++ src/lab4.java
@@ -5,6 +5,8 @@
 //example(非整除): 2^9 = 2^4 * [x] * [y]其中[x]屬於2^4;[y]屬於2,因此必須定義N/2整除時的case與非整除時的case
 
 
+import org.omg.PortableInterceptor.SYSTEM_EXCEPTION;
+
 import java.util.Scanner;
 
 public class lab4 {
@@ -21,16 +23,43 @@ public class lab4 {
         }
         return n;
     }
+
+    public static float power1(float x, int n){
+        if (n > 0){
+            double check = n / 2;
+            System.out.println("check:" + check);
+            double y = power1(x , n / 2);
+            System.out.println("before:" + y);
+            y = y * y;
+            System.out.println("after:" + y);
+
+//            n = n-1;
+//            return (x*power(x,n));
+//        }else if(n == 0 ){
+//            return 1;
+//        }else if(n < 0){
+//            int y = Math.abs(n);
+//            return (1 / power(x,y));
+        }
+        return n;
+    }
+
+
     public static void main(String[] args) {
 
-        Scanner input = new Scanner(System.in);
-        System.out.println("Please input x and n, x ^ n");
-        int x = input.nextInt();
-        int n = input.nextInt();
+//        Scanner input = new Scanner(System.in);
+//        System.out.println("Please input x and n, x ^ n");
+//        int x = input.nextInt();
+//        int n = input.nextInt();
+//
+//        double z = power(x,n);
+//        System.out.println(z);
+//        input.close();
 
-        double z = power(x,n);
-        System.out.println(z);
-        input.close();
+        double test = power1(2,5);
+        System.out.println(test);
+
+
     }
 
 }
